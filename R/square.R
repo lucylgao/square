887a827dae1ecdad34a05604ed1aef9b893285d9
Lucy Gao <lucystatsca@gmail.com>
2022-11-03 09:51:28 -0700

    Added square() function with documentation and minimal testing

---
+++ R/square.R
@@ -0,0 +1,13 @@
+#' Squares a number or a vector of numbers
+#'
+#' @param x a number or a numeric vector to be squared
+#'
+#' @return the square of x
+#' @export
+#'
+#' @examples
+#' square(10) # should be 100
+#' square(c(2, 4)) # should be c(4, 16)
+square <- function(x) {
+    x^2
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(square)
+
+test_check("square")
